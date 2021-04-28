9b309b72b488f20567f6ec6741b3c1f964c3ccba
Hervinder <hbhandal80@hotmail.com>
2021-04-28 21:06:08 +0000

    eslinted version

---
+++ middle.js
@@ -0,0 +1,51 @@
+const eqArrays = function(array1, array2) {
+
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i ++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  if (eqArrays(actual, expected)) {
+    //console.log("🍷🍷 " + "Assertion Passed: " + [actual] + " === " + [expected]);
+    console.log(`🍷🍷 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    //console.log("❌❌ " + "Assertion Failed : " + [actual] + " !== " + [expected]);
+    console.log(`❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const middle = function(array) {
+  
+  if (array.length <= 2) {
+    return [];
+  } if (array.length % 2 === 0) {
+    return [array[Math.floor(array.length / 2) - 1], array[Math.floor(array.length / 2)]];
+  } else {
+    return array[Math.floor(array.length / 2)];
+  }
+};
+
+// TEST CODE
+
+console.log(middle([1, 3]));
+console.log(middle([3]));
+console.log("---");
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log("-----");
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+console.log("-------");
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
