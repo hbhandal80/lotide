4e9dcc0ef499df2b63347b4df5a49ae56e886954
Hervinder <hbhandal80@hotmail.com>
2021-04-29 05:04:56 +0000

    Linted version. Not outputting 2 same letters

---
+++ letterPositions.js
@@ -0,0 +1,36 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  if (eqArrays(actual, expected)) {
+    //console.log("🍷🍷 " + "Assertion Passed: " + [actual] + " === " + [expected]);
+    console.log(`🍷🍷 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    //console.log("❌❌ " + "Assertion Failed : " + [actual] + " !== " + [expected]);
+    console.log(`❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+console.log(letterPositions('hello'));
+assertArraysEqual(letterPositions("hello").e, [1]);
