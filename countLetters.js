0d041e33c30a3fce03a590f3a51c4425f6ea36ac
Hervinder <hbhandal80@hotmail.com>
2021-04-29 04:04:03 +0000

    Linted version

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    //console.log("🍷🍷 " + "Assertion Passed: " + [actual] + " === " + [expected]);
+    console.log(`🍷🍷 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    //console.log("❌❌ " + "Assertion Failed : " + [actual] + " !== " + [expected]);
+    console.log(`❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const count = {};
+  
+  for (const char of string) {
+    if (count[char]) {
+      count[char] += 1;
+    } else {
+      count[char] = 1;
+    }
+  }
+  return count;
+};
+
+console.log(countLetters("Lighthouse in the house"));
+  
