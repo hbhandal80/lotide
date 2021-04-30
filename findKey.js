d2a166a954e6eaced2796316d09315c3df067e25
Hervinder <hbhandal80@hotmail.com>
2021-04-30 04:05:00 +0000

    eslinted version

---
+++ findKey.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    //console.log("🍷🍷 " + "Assertion Passed: " + [actual] + " === " + [expected]);
+    console.log(`🍷🍷 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    //console.log("❌❌ " + "Assertion Failed : " + [actual] + " !== " + [expected]);
+    console.log(`❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(array, callback) {
+  for (const key in array) {
+    if (callback(array[key])) {
+      return key;
+    }
+  }
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma")
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3)); // => "Akaleri")
+
