293b8fa4fbd565c2996cac14a694274374a5a7aa
Hervinder <hbhandal80@hotmail.com>
2021-05-04 02:16:58 +0000

    refactored

---
+++ test/tailTest.js
@@ -0,0 +1,5 @@
+const tail = require('../tail');
+const assertEqual = require('../assertEqual');
+
+assertEqual(tail([5,6,7], [6,7]));
+assertEqual(tail(["Hello", "Lighthouse", "Labs"], ["Lighthouse", "Labs"]));
