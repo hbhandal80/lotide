b973e7b15e756d92a927b85f8ecc037777cb46ab
Hervinder <hbhandal80@hotmail.com>
2021-05-04 01:21:07 +0000

    Refactored

--- assertEqual.js
+++ assertEqual.js
@@ -1,16 +1,10 @@
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    //console.log("🍷🍷 " + "Assertion Passed: " + [actual] + " === " + [expected]);
     console.log(`🍷🍷 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    //console.log("❌❌ " + "Assertion Failed : " + [actual] + " !== " + [expected]);
     console.log(`❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Hervinder", "Bhandal");
-assertEqual("Vancouver", "Vancouver");
-assertEqual(21, 15);
+module.exports = assertEqual;
